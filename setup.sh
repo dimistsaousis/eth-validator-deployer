9a3401312a60192fb72643917c4beb4d3bbe6b3d
Dimis Tsaousis <dimistsaousis@gmail.com>
2023-10-03 18:11:14 +0300

    setup script

---
+++ setup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Check if running as root
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run this script as root."
+    exit 1
+fi
+
+# Create jwtsecrtet
+openssl rand -hex 32 | tr -d "\n" > jwtsecret
+
+# Update the package database
+apt update
+
+# Install required dependencies
+apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+# Check if Docker is installed
+if ! command -v docker &>/dev/null; then
+    # Install Docker
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
+    apt update
+    apt install -y docker-ce docker-ce-cli containerd.io
+
+    # Start and enable Docker service
+    systemctl start docker
+    systemctl enable docker
+fi
+
+# Check if Docker Compose is installed
+if ! command -v docker-compose &>/dev/null; then
+    # Install Docker Compose
+    curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+fi
+
+# Verify installations
+docker --version
+docker-compose --version
+
+# Build Docker Compose services
+docker-compose build
+
+echo -e "${GREEN}Docker and Docker Compose have been installed and services have been built successfully.${NC}"
