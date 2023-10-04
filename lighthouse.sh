efb78b2b9d5167bd4d9a12d3429709eaba9468ed
Dimis Tsaousis <dimistsaousis@gmail.com>
2023-10-04 14:58:23 +0300

    remove unecessary stuff

--- lighthouse.sh
+++ lighthouse.sh
@@ -1,32 +1,14 @@
 #!/bin/bash
 
 # Define variables
-lighthouse_path="/usr/local/bin/lighthouse"  # Replace with the actual path to the lighthouse executable
-validator_dir="/var/lib/lighthouse/keys"  # Directory to store validator keys
-password="mypassword"
+lighthouse_path="/usr/local/bin/lighthouse"
 network="goerli"
-
-# Create the keys directory if it doesn't exist
-mkdir -p "$validator_dir"
-
-# Check if validator keys already exist
-if [ ! -f "$validator_dir/validator_keystore.json" ]; then
-    echo "Validator keys do not exist. Generating..."
-    "$lighthouse_path" account validator create \
-      --network "$network" \
-      --output-dir "$validator_dir" \
-      --count 1 \
-      --password "$password"
-
-fi
 
 # Start the Validator process in the background and capture its output to a log file
 "$lighthouse_path" validator_client \
   --network "$network" \
   --datadir /var/lib/lighthouse \
   --graffiti "Erdos" \
-  --validators-dir "$validator_dir" \
-  --wallet-password "$password" \
   > /var/lib/lighthouse/validator.log 2>&1 &
 
 # Start the Beacon process and capture its output to a log file
