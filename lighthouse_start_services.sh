e7dbb7e90db1b3277fdfeea9d8857d34a7b186be
Dimis Tsaousis <dimistsaousis@gmail.com>
2023-10-04 16:50:09 +0300

    expose relevant ports on erigon

--- lighthouse_start_services.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Start the systemd init system
-exec /lib/systemd/systemd &
-
-# Capture and output systemd journal logs to stdout
-journalctl -u lighthousebeacon.service -u lighthousevalidator.service -f
