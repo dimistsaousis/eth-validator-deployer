6ce8512a59a3e830f41bea3afad29cf1877bac1a
Dimis Tsaousis <dimistsaousis@gmail.com>
2023-10-07 04:54:01 +0300

    keymanager script : store keystore password

--- keymanager.sh
+++ keymanager.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+set -Eeuo pipefail
 
-echo "$KEYSTORE_PASSWORD" > keys/password-file
+if [ "$(id -u)" = '0' ]; then
+  chown -R lhvalidator:lhvalidator /var/lib/lighthouse
+  chown -R lhvalidator:lhvalidator /keys
+  exec gosu lhvalidator docker-entrypoint.sh "$@"
+fi
+
+echo "$KEYSTORE_PASSWORD" > /keys/password-file
+chown lhvalidator:lhvalidator /keys/password-file
+chmod 700 keys/password-file
+
 lighthouse --network goerli account validator import --directory /keys --datadir /var/lib/lighthouse --password-file /keys/password-file --reuse-password
+
 exec "$@"
