22de9bd7d8220aa15777ad43724aafefcd640563
Dimis Tsaousis <dimistsaousis@gmail.com>
2023-10-07 04:56:25 +0300

    rename keys directory to keystore for clarity

--- keymanager.sh
+++ keymanager.sh
@@ -3,14 +3,14 @@ set -Eeuo pipefail
 
 if [ "$(id -u)" = '0' ]; then
   chown -R lhvalidator:lhvalidator /var/lib/lighthouse
-  chown -R lhvalidator:lhvalidator /keys
+  chown -R lhvalidator:lhvalidator /keystore
   exec gosu lhvalidator docker-entrypoint.sh "$@"
 fi
 
-echo "$KEYSTORE_PASSWORD" > /keys/password-file
-chown lhvalidator:lhvalidator /keys/password-file
+echo "$KEYSTORE_PASSWORD" > /keystore/password-file
+chown lhvalidator:lhvalidator /keystore/password-file
 chmod 700 keys/password-file
 
-lighthouse --network goerli account validator import --directory /keys --datadir /var/lib/lighthouse --password-file /keys/password-file --reuse-password
-rm -r /keys
+lighthouse --network goerli account validator import --directory /keystore --datadir /var/lib/lighthouse --password-file /keystore/password-file --reuse-password
+rm -r /keystore
 exec "$@"
