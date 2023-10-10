8b866a24669251e561d87b1644598708b6b8fb20
Dimis Tsaousis <dimistsaousis@gmail.com>
2023-10-10 18:39:22 +0300

    adding some comments on the scripts

--- scripts/docker-volume-size.sh
+++ scripts/docker-volume-size.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# Get a list of all docker volume names
+# Get a list of all docker volume names and their sizes
 volumes=$(sudo docker volume ls --format "{{.Name}}")
 
 for volume in $volumes; do
-  # Get the mountpoint of the volume
   mountpoint=$(sudo docker inspect --format '{{ .Mountpoint }}' $volume)
-  
-  # Get the size of the volume
   size=$(sudo du -sh $mountpoint | cut -f1)
-  
   echo "Volume: $volume, Size: $size"
 done
--- scripts/erigon-sync-status.sh
+++ scripts/erigon-sync-status.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# Get the sync status of the erigon node
+
 docker exec -it execution-container curl -X POST --data '{"jsonrpc":"2.0","method":"eth_syncing","params":[],"id":1}' -H "Content-Type: application/json" http://localhost:8545
